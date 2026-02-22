2bdd84580e6d5b4a7337841a5f1090a90be95a36
Ghanta Sri Kameshu <srikaeshghanta22@gmail.com>
2026-02-22 21:57:04 +0530

    Use environment database URL for production

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:plsql@localhost:5432/maize_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
